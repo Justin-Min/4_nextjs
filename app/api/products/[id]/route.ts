d14432d6950e63a502ede06ee8e694bcb794f354
Justin-Min <sa3333@naver.com>
2024-12-10 21:46:38 +0900

    12/10 21:46

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,6 +1,38 @@
 import { NextRequest } from 'next/server'
+import { products } from '@/app/data/productData'
+import prisma from '@/prisma/db_client'
 
 export async function PUT(
 	req: NextRequest,
 	{ params }: { params: { id: string } }
-) {}
+) {
+	const body = await req.json()
+	const { title, description, price, category, img } = body
+
+	const UpdatedProduct = await prisma.product.update({
+		where: {
+			id: parseInt(params.id),
+		},
+		data: {
+			title: title,
+			price: price,
+			description: description,
+			category: category,
+			img: img,
+		},
+	})
+
+	return Response.json(UpdatedProduct, { status: 200 })
+}
+
+export async function DELETE(
+	req: NextRequest,
+	{ params }: { params: { id: string } }
+) {
+	const productIndex = products.findIndex(
+		product => product.id == parseInt(params.id)
+	)
+	products.splice(productIndex, 1)[0]
+
+	return Response.json('Delete successfully', { status: 200 })
+}
--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,22 +1,22 @@
-import { products } from '@/app/data/productData'
 import { NextRequest } from 'next/server'
+import prisma from '@/prisma/db_client'
 
 export async function GET() {
+	const products = await prisma.product.findMany()
 	return Response.json(products)
 }
 
 export async function POST(req: NextRequest) {
 	const body = await req.json()
+	const newProduct = await prisma.product.create({
+		data: {
+			title: body.title,
+			price: body.price,
+			description: body.description,
+			category: body.category,
+			img: body.img,
+		},
+	})
 
-	const newProduct = {
-		id: products.length + 1,
-		title: body.title,
-		description: body.description,
-		price: body.price,
-		category: body.category,
-		image: body.image,
-	}
-
-	products.push(newProduct)
 	return Response.json(newProduct, { status: 201 })
 }
